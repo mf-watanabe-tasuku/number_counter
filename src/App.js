d327518dc6ef3bd8f215087387338172a9235fbb
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2023-09-30 01:29:05 +0900

    add click up functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const targetNums = [...Array(9).keys()].map(num => num + 1);
+
+  const [clickedNums, setClickedNums] = useState([]);
+
+  useEffect(() => {
+    if (clickedNums.length === targetNums.length) {
+      const matchedNums = clickedNums.filter((cnum, i) => cnum === targetNums[i]);
+
+      if (matchedNums.length === targetNums.length) {
+        console.log('success!');
+      }
+
+      setClickedNums([]);
+    }
+  }, [clickedNums]);
+
+  const numClick = (e) => {
+    console.log(e.target.textContent);
+    setClickedNums([...clickedNums, parseInt(e.target.textContent)]);
+  }
+
   return (
-    <div>Hello World</div>
+    <>
+      <ul className='num-list'>
+        {targetNums.map(num => {
+          return <li key={num} className="num-item" onClick={numClick}>{num}</li>
+        })}
+      </ul>
+    </>
   );
 }
 
