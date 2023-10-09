8d53b7193d56da84065c9185582549b7d219e977
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2023-10-10 00:04:20 +0900

    delete NumItem component

--- src/components/NumItem.jsx
+++
@@ -1,8 +0,0 @@
-import { memo } from "react";
-
-const NumItem = memo(({num, clicked, numClick}) => {
-  console.log('NumItem rendered');
-  return <li className={`num-item${clicked ? " clicked" : ""}`} onClick={numClick}>{num}</li>;
-});
-
-export default NumItem;
--- src/components/NumList.jsx
+++ src/components/NumList.jsx
@@ -1,13 +1,12 @@
 import { memo } from "react";
 import { nanoid } from 'nanoid';
-import NumItem from "./NumItem";
 
 const NumList = memo(({numObjects, numClick}) => {
     console.log('NumList rendered');
     return (
       <ul className="num-list">
         {numObjects.map(({num, clicked}) => {
-          return <NumItem key={nanoid()} num={num} clicked={clicked} numClick={numClick} />
+          return <li key={nanoid()} className={`num-item${clicked ? " clicked" : ""}`} onClick={numClick}>{num}</li>
         })}
       </ul>
     );
